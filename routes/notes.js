341ac98fceadb08bbb3efb7bc62ce02fa5c8eaa3
Kanako Takeyama <newroad1702_leshvo14@outlook.jp>
2023-12-13 02:46:59 +0900

    catAPI dogAPI

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "*******";
+const uri = "***********";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {

1e1d36c5402dac37ccd611343ce935014a1f7196
zubolander <newroad1702_leshvo14@outlook.jp>
2024-02-01 11:55:42 +0900

    routesNewfils

---
+++ routes/notes.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "***";
+const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+
+res.json(note);
+})
+
+module.exports = router;
---
+++ routes/notes_from_b.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
+
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "**";
+const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
+
+res.json(note);
+})
+
+module.exports = router;

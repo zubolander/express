a669fa60045e28316d704b3e55db9fd5bf90888e
zubolander <newroad1702_leshvo14@outlook.jp>
2024-02-08 11:19:01 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "**";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
